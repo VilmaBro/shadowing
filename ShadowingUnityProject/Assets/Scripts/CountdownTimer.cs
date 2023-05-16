cd9a23808d6062e3b05fab9c6e59b8bb52c7f603
Lunalixx <felice.z.t@gmail.com>
2023-05-16 18:47:30 +0200

    CountdownTimer

---
+++ ShadowingUnityProject/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    private float timerDuration = 3f * 60F;
+
+    void Start(){
+
+    }
+
+
+    void Update(){
+        
+    }
+
+       
+
+}
